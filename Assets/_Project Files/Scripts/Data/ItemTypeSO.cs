4b33878f3504c385333e479593e932f78459fede
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2024-01-16 08:06:11 -0500

    updates to folder structure

--- Assets/_Project Files/Scripts/Data/ItemTypeSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Item Type", menuName = "Items/Item/New Item Type")]
-public class ItemTypeSO : ScriptableObject
-{
-    public string itemCategory; // Weapon, Material, Shield
-    public string itemDescription;
-}
