26c21267ab15f7f13894ea0bafff8c0a4aa97b83
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2024-01-28 09:47:22 -0500

    minor updates

--- Assets/Scripts/Scriptable Objects/PlayerData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Data/Player/Player data", fileName = "New Player data")]
-public class PlayerData : ScriptableObject
-{
-    GameObject _player;
-}
