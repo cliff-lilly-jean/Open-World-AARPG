81900b7c3d52bbc409d9908ff7fdd18cd38fe5cc
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2024-02-11 13:57:40 -0500

    movement updates

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -4,10 +4,31 @@ using UnityEngine.InputSystem;
 public class PlayerMovementController : MonoBehaviour
 {
     private Vector2 _inputVector;
+    private Rigidbody _rb;
+
+    private float _movementX;
+    private float _movementY;
+
+    public float _speed;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(_movementX, 0f, _movementY);
+        _rb.AddForce(movement * _speed);
+        Debug.Log(movement);
+    }
 
     public void Move(InputAction.CallbackContext context)
     {
         _inputVector = context.ReadValue<Vector2>();
-        Debug.Log(_inputVector);
+
+        _movementX = _inputVector.x;
+        _movementY = _inputVector.y;
+        // Debug.Log(_inputVector);
     }
 }
