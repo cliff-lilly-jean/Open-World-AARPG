29308a1718597813b450cf4e1e1733d65bbf2200
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2024-02-13 00:09:07 -0500

    Added look direction  and rotation to the playerMovmentController

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,11 +6,15 @@ public class PlayerMovementController : MonoBehaviour
 {
     private Vector2 _inputVector;
     private Rigidbody _rb;
+    private Vector3 _direction;
 
-    private float _movementX;
-    private float _movementY;
 
-    public float _speed;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _smoothTime = 0.05f;
+
+    private float _currentVelocity;
 
     private void Awake()
     {
@@ -18,17 +23,42 @@ public class PlayerMovementController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(_movementX, 0f, _movementY);
-        _rb.AddForce(movement * _speed);
-        Debug.Log(movement);
+
+        if (_inputVector.sqrMagnitude == 0) return;
+        Walk();
     }
 
     public void Move(InputAction.CallbackContext context)
     {
+        // Get input
         _inputVector = context.ReadValue<Vector2>();
 
-        _movementX = _inputVector.x;
-        _movementY = _inputVector.y;
-        // Debug.Log(_inputVector);
+        // Convert input variables
+        _direction.x = _inputVector.x;
+        _direction.y = _inputVector.y;
+    }
+
+
+    public void Walk()
+    {
+        LookRotation();
+        Vector3 movement = new Vector3(_direction.x, 0f, _direction.y);
+        _rb.AddForce(movement * _speed);
+    }
+
+    public void LookRotation()
+    {
+        // Get direction angles
+        var lookDirection = Mathf.Atan2(_direction.x, _direction.y) * Mathf.Rad2Deg;
+        var rotationAngle = RotationSmoothing(lookDirection);
+
+        // Rotate the transform in the lookDirection
+        transform.rotation = Quaternion.Euler(0f, rotationAngle, 0f);
+    }
+
+    public float RotationSmoothing(float direction)
+    {
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, direction, ref _currentVelocity, _smoothTime);
+        return angle;
     }
 }
