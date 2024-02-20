08ac1de7323e924579341c53e35de16bb85d3c22
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2024-02-20 00:09:02 -0500

    Controller updates

---
+++ Assets/Scripts/Input/HumanoidLandInput.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HumanoidLandInput : MonoBehaviour
+{
+
+    public Vector2 moveInput { get; private set; } = Vector2.zero;
+    public Vector2 lookInput { get; private set; } = Vector2.zero;
+
+    Controls _controls = null;
+
+
+    private void OnEnable()
+    {
+        _controls = new Controls();
+        _controls.HumanoidLand.Enable();
+
+        // Move
+        _controls.HumanoidLand.Move.performed += SetMove;
+        _controls.HumanoidLand.Move.canceled += SetMove;
+
+        _controls.HumanoidLand.Look.performed += SetLook;
+        _controls.HumanoidLand.Look.canceled += SetLook;
+    }
+
+    private void OnDisable()
+    {
+        // Move
+        _controls.HumanoidLand.Move.performed -= SetMove;
+        _controls.HumanoidLand.Move.canceled -= SetMove;
+
+        // Look
+        _controls.HumanoidLand.Look.performed -= SetLook;
+        _controls.HumanoidLand.Look.canceled -= SetLook;
+
+        _controls.HumanoidLand.Disable();
+    }
+
+    private void SetMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void SetLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+}
