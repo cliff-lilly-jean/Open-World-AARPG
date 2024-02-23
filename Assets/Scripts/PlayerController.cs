ce940f826dd5a509f44ccbb79cbbaa38395c072d
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2024-02-23 18:35:46 -0500

    sprint mechanic implemented. Need to add stamina and stamina wheel

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     GameControls controls;
+    bool _sprint;
 
     private Vector2 _direction;
     private Rigidbody _rb;
@@ -13,10 +14,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _jumpStrength;
     [SerializeField] private float _smoothTime = 0.05f;
     [SerializeField] private float _gravityForce = 6f;
+    [SerializeField] private float _speedBoost;
 
     private float _currentVelocity;
     private float _groundCheck;
-    // private float _defaultSpeed;
+    private float _defaultSpeed;
     private float _force = 100;
 
     private float _bufferDistance = 0.1f;
@@ -32,11 +34,19 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _defaultSpeed = _speed;
 
         controls = new GameControls();
+
+        // Jump
         controls.Gameplay.Jump.performed += _ => Jump();
+
+        // Move
         controls.Gameplay.Move.performed += _ => Move();
-        controls.Gameplay.Sprint.performed += ctx => Sprint(ctx); // TODO: Maybe don't sync this to the performed event, find a way to get the _isSprinting variable to toggle
+
+        // Sprint
+        controls.Gameplay.Sprint.performed += _ => Sprint();
+        controls.Gameplay.Sprint.canceled += _ => SprintCanceled();
     }
 
     private void Update()
@@ -54,12 +64,6 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if (!_isSprinting)
-        {
-            Debug.Log("aint sprinting");
-        }
-
-        Debug.Log(_isSprinting);
     }
 
     private void FixedUpdate()
@@ -127,10 +131,23 @@ public class PlayerController : MonoBehaviour
     #endregion Jump
 
     #region Sprint
-    private bool Sprint(InputAction.CallbackContext context)
+    private void Sprint()
     {
-        return _isSprinting = context.performed ? true : false;
+        // return _isSprinting = context.performed ? true : false;
+        Debug.Log("Sprint");
+        _isSprinting = true;
+        _speed = _speed + _speedBoost;
+        Debug.Log("Current Speed pressed: " + _speed);
     }
+
+    private void SprintCanceled()
+    {
+        Debug.Log("Canceled");
+        _isSprinting = false;
+        _speed = _defaultSpeed;
+        Debug.Log("Current Speed not pressed: " + _speed);
+    }
+
     #endregion
 
     public void OnEnable()
