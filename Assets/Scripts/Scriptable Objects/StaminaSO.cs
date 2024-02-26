87d3552780a6c0f380bb87f58f13272e4b987205
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2024-02-26 10:45:57 -0500

    Added a Stamina scriptable object, need to incorporate stamina system in to player controller

--- Assets/Scripts/Scriptable Objects/StaminaSO.cs
+++ Assets/Scripts/Scriptable Objects/StaminaSO.cs
@@ -1,11 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Systems/Stamina")]
+[CreateAssetMenu(menuName = "Systems/Movement/Stamina")]
 public class StaminaSO : ScriptableObject
 {
     public MovementSystemSO movementSystem;
 
     public float _stamina;
     public float _maxStamina = 100f;
+
+    public bool _staminaExhausted;
+
+    // Stamina Wheel
     public float _wheelFillSpeedOffset;
+
+    private void OnEnable()
+    {
+        _stamina = _maxStamina;
+    }
 }
--- Assets/Scripts/UI/StaminaWheel.cs
+++ Assets/Scripts/UI/StaminaWheel.cs
@@ -3,66 +3,52 @@ using UnityEngine.UI;
 
 public class StaminaWheel : MonoBehaviour
 {
-    public MovementSystemSO movementSystem;
-
-    private float _stamina;
-    private float _maxStamina = 100f;
-
-    [SerializeField] private float _wheelFillSpeedOffset;
+    public StaminaSO stamina;
 
     [SerializeField] private Image _greenWheel;
     [SerializeField] private Image _redWheel;
 
-    private bool _staminaExhausted;
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _stamina = _maxStamina;
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (movementSystem.move.isSprinting && !_staminaExhausted)
+        if (stamina.movementSystem.move.isSprinting && !stamina._staminaExhausted)
         {
-            if (_stamina > 0)
+            if (stamina._stamina > 0)
             {
                 _greenWheel.enabled = true;
                 _redWheel.enabled = true;
 
-                _stamina -= 15 * Time.deltaTime;
+                stamina._stamina -= 15 * Time.deltaTime;
             }
             else
             {
                 _greenWheel.enabled = false;
-                _staminaExhausted = true;
+                stamina._staminaExhausted = true;
                 // TODO make a glow effect when stamina is exhausted
             }
 
-            _redWheel.fillAmount = (_stamina / _maxStamina + (_wheelFillSpeedOffset + .005f));
+            _redWheel.fillAmount = (stamina._stamina / stamina._maxStamina + (stamina._wheelFillSpeedOffset + .005f));
         }
         else
         {
-            if (_stamina < _maxStamina)
+            if (stamina._stamina < stamina._maxStamina)
             {
-                _stamina += 10 * Time.deltaTime;
+                stamina._stamina += 10 * Time.deltaTime;
             }
             else
             {
-                _staminaExhausted = false;
+                stamina._staminaExhausted = false;
 
                 // Hide wheels until isSprinting
                 _greenWheel.enabled = false;
                 _redWheel.enabled = false;
             }
 
-            _redWheel.fillAmount = (_stamina / _maxStamina + (_wheelFillSpeedOffset + .005f));
+            _redWheel.fillAmount = (stamina._stamina / stamina._maxStamina + (stamina._wheelFillSpeedOffset + .005f));
         }
 
-        _greenWheel.fillAmount = (_stamina / _maxStamina);
+        _greenWheel.fillAmount = (stamina._stamina / stamina._maxStamina);
     }
 }
 
