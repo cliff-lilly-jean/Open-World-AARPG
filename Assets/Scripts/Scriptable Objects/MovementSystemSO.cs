32902394da85d1bddcfcd6722c7ebe785afea6b1
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2024-02-26 15:22:54 -0500

    reverted the movement system

--- Assets/Scripts/Scriptable Objects/MovementSystemSO.cs
+++ Assets/Scripts/Scriptable Objects/MovementSystemSO.cs
@@ -4,13 +4,12 @@ using UnityEngine.InputSystem;
 [CreateAssetMenu(menuName = "Systems/Movement/Movement System")]
 public class MovementSystemSO : ScriptableObject
 {
-    public GameControls controls;
-    public Rigidbody _rb;
 
     public MoveSO move;
     public JumpSO jump;
 
     public float force = 100;
 
-    // Use OnEnable to initialize Image, Transforms, Gameobjects
+    // Use OnEnable to initialize Image, Transforms, GameObjects
+
 }
