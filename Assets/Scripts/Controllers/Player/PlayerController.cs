a8e4b558b0561647cd8f0f5028fcd70e3b77847b
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2024-03-03 14:52:39 -0500

    updates

--- Assets/Scripts/Controllers/Player/PlayerController.cs
+++ Assets/Scripts/Controllers/Player/PlayerController.cs
@@ -5,7 +5,12 @@ public class PlayerController : MonoBehaviour
 {
     public GameControls controls;
     public Rigidbody _rb;
+    MovementSystem movementSystem;
 
+    public Move move;
+    public Run run;
+    public Stamina stamina;
+    public Jump jump;
 
 
     private void Awake()
@@ -16,11 +21,30 @@ public class PlayerController : MonoBehaviour
         // Rigidbody
         _rb = GetComponent<Rigidbody>();
 
+        // Controllers
+        move = GetComponent<Move>();
+        run = GetComponent<Run>();
+        jump = GetComponent<Jump>();
+        stamina = GetComponent<Stamina>();
+
+        // Controls
+        controls.Gameplay.Walk.performed += ctx => move.Activate();
+
+        controls.Gameplay.Jump.performed += ctx => jump.Activate(ctx);
+
+        controls.Gameplay.Run.performed += ctx => run.Activate();
+        controls.Gameplay.Run.canceled += ctx => run.Cancel();
     }
 
     private void Start()
     {
 
+    }
+
+    private void FixedUpdate()
+    {
+        // if (movementSystem.walk.moveDirection.sqrMagnitude == 0) { };
+        // move.Activate();
     }
 
     public void OnEnable()
